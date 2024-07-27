2cb7d34e233a286c2104c583cede4d6f0214a913
Sagnik0000 <159174372+Sagnik0000@users.noreply.github.com>
2024-07-27 23:20:23 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-target]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+        tabContent.classList.remove('tab__active');
+    });
+
+    target.classList.add('tab__active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('tab__active');
+    });
+
+    tab.classList.add('tab__active');
+  });  
+});
